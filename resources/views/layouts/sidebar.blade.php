7bc5c65a9b2d65b43c64e72fad24b9618b58ccd7
Mirandadws <mirandadws1464@gmail.com>
2025-04-18 22:25:01 +0700

    Update sidebar.blade.php
    jarak poli umum

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -100,7 +100,7 @@
             <div class="round-16 d-flex align-items-center justify-content-center">
                 <i class="ti ti-circle"></i>
             </div>
-            <span class="hide-menu">Poli Umum</span>
+            <span class="hide-menu ms-3">Poli Umum</span>
         </div>
         <i class="ti ti-chevron-down ms-auto"></i> {{-- ini panahnya --}}
     </a>
