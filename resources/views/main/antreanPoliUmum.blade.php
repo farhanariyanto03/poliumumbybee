b0d7d65efeb2df7641b30c1cd838bca696e44933
Mirandadws <mirandadws1464@gmail.com>
2025-04-23 22:46:27 +0700

    Update antreanPoliUmum.blade.php

--- resources/views/main/antreanPoliUmum.blade.php
+++ resources/views/main/antreanPoliUmum.blade.php
@@ -3,50 +3,45 @@
 @section('title', 'SIP-Kes')
 
 @section('pageContent')
-<div class="container my-4">
-    <h2 class="text-center fw-bold" style="font-size: 2.5rem; color: #1A1A1A;">Antrean Poli Umum</h2>
+<h1 class="fw-bold text-primary mb-4">Antrean Poli Umum</h1>
 
-    <div class="d-flex justify-content-end mb-3">
-        <div class="input-group" style="max-width: 300px;">
-            <input type="text" class="form-control" placeholder="Data Pasien">
-            <button class="btn btn-primary" type="button">
-                <i class="ti ti-search"></i> {{-- Sesuaikan dengan icon library kamu --}}
-            </button>
-        </div>
-    </div>
+<div class="card p-4 shadow">
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>NO ANTRIAN</th>
+                <th>NOMOR RM</th>
+                <th>NAMA</th>
+                <th>NIK</th>
+                <th>Tanggal Lahir</th>
+                <th>TANGGAL PERIKSA</th>
+                <th>UNIT LAYANAN</th>
+                <th>DOKTER</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $antreanPoliUmum = [
+                    ['no_rm' => '001001', 'nama' => 'Arga Pratama', 'nik' => '3590168908650001', 'tgl_lahir' => '01-03-2004', 'tgl_periksa' => '17-04-2025', 'layanan' => 'Umum', 'dokter' => 'dr. Oktavia'],
+                    ['no_rm' => '001002', 'nama' => 'Keisha Anindya', 'nik' => '359578436678975', 'tgl_lahir' => '16-12-2000', 'tgl_periksa' => '17-04-2025', 'layanan' => 'Umum', 'dokter' => 'dr. Erwiyan'],
+                    // Tambah data lain sesuai kebutuhan
+                ];
+            @endphp
 
-    <div class="card shadow p-3">
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover align-middle text-center">
-                <thead class="table-light">
-                    <tr>
-                        <th>NO ANTRIAN</th>
-                        <th>NOMOR RM</th>
-                        <th>NAMA</th>
-                        <th>NIK</th>
-                        <th>Tanggal Lahir</th>
-                        <th>TANGGAL PERIKSA</th>
-                        <th>UNIT LAYANAN</th>
-                        <th>DOKTER</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {{-- Contoh data statis. Nanti ini diganti dari controller --}}
-                    @foreach ($antreanPoliUmum as $a)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $a->no_rm }}</td>
-                            <td>{{ $a->nama }}</td>
-                            <td>{{ $a->nik }}</td>
-                            <td>{{ $a->tanggal_lahir->format('d-m-Y') }}</td>
-                            <td>{{ $a->tanggal_periksa->format('d-m-Y') }}</td>
-                            <td>{{ $a->unit_layanan }}</td>
-                            <td>{{ $a->dokter }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+            @foreach ($antreanPoliUmum as $index => $a)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $a['no_rm'] }}</td>
+                    <td>{{ $a['nama'] }}</td>
+                    <td>{{ $a['nik'] }}</td>
+                    <td>{{ $a['tgl_lahir'] }}</td>
+                    <td>{{ $a['tgl_periksa'] }}</td>
+                    <td>{{ $a['layanan'] }}</td>
+                    <td>{{ $a['dokter'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
+
 @endsection
