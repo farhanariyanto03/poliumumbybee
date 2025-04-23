9ce3d95c58210730735f64289f128b8045064820
Mirandadws <mirandadws1464@gmail.com>
2025-04-24 01:59:06 +0700

    Update antreanPoliUmum.blade.php

--- resources/views/main/antreanPoliUmum.blade.php
+++ resources/views/main/antreanPoliUmum.blade.php
@@ -3,9 +3,7 @@
 @section('title', 'SIP-Kes')
 
 @section('pageContent')
-@extends('layouts.app')
 
-@section('content')
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Poppins:wght@400;600&display=swap');
 
