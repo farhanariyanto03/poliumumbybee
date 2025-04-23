2be6b78d3f2baf832054f8adb4daf24fadfb4b26
Mirandadws <mirandadws1464@gmail.com>
2025-04-24 00:10:41 +0700

    Update antreanPoliUmum.blade.php

--- resources/views/main/antreanPoliUmum.blade.php
+++ resources/views/main/antreanPoliUmum.blade.php
@@ -3,45 +3,65 @@
 @section('title', 'SIP-Kes')
 
 @section('pageContent')
-<h1 class="fw-bold text-primary mb-4">Antrean Poli Umum</h1>
+@extends('layouts.app')
 
-<div class="card p-4 shadow">
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>NO ANTRIAN</th>
-                <th>NOMOR RM</th>
-                <th>NAMA</th>
-                <th>NIK</th>
-                <th>Tanggal Lahir</th>
-                <th>TANGGAL PERIKSA</th>
-                <th>UNIT LAYANAN</th>
-                <th>DOKTER</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $antreanPoliUmum = [
-                    ['no_rm' => '001001', 'nama' => 'Arga Pratama', 'nik' => '3590168908650001', 'tgl_lahir' => '01-03-2004', 'tgl_periksa' => '17-04-2025', 'layanan' => 'Umum', 'dokter' => 'dr. Oktavia'],
-                    ['no_rm' => '001002', 'nama' => 'Keisha Anindya', 'nik' => '359578436678975', 'tgl_lahir' => '16-12-2000', 'tgl_periksa' => '17-04-2025', 'layanan' => 'Umum', 'dokter' => 'dr. Erwiyan'],
-                    // Tambah data lain sesuai kebutuhan
-                ];
-            @endphp
+@section('content')
+<div class="container py-4">
+    <h1 class="mb-4" style="color: #111754; font-weight: 700; font-size: 2.5rem;">Antrean Poli Umum</h1>
 
-            @foreach ($antreanPoliUmum as $index => $a)
-                <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $a['no_rm'] }}</td>
-                    <td>{{ $a['nama'] }}</td>
-                    <td>{{ $a['nik'] }}</td>
-                    <td>{{ $a['tgl_lahir'] }}</td>
-                    <td>{{ $a['tgl_periksa'] }}</td>
-                    <td>{{ $a['layanan'] }}</td>
-                    <td>{{ $a['dokter'] }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    {{-- Search Bar --}}
+    <div class="mb-3 d-flex justify-content-end">
+        <input type="text" class="form-control w-25 me-2" placeholder="Data Pasien">
+        <button class="btn btn-primary"><i class="ti ti-search"></i></button>
+    </div>
+
+    {{-- Table --}}
+    <div class="card p-3 shadow-sm">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead style="background-color: #F9FAFC; border: 1px solid #B9B9B9;">
+                    <tr>
+                        <th>NO ANTRIAN</th>
+                        <th>NOMOR RM</th>
+                        <th>NAMA</th>
+                        <th>NIK</th>
+                        <th>Tanggal Lahir</th>
+                        <th>TANGGAL PERIKSA</th>
+                        <th>UNIT LAYANAN</th>
+                        <th>DOKTER</th>
+                        <th>TIPE PASIEN</th>
+                        <th>STATUS</th>
+                        <th>AKSI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{-- Simulasi data dummy --}}
+                    @foreach (range(1, 10) as $i)
+                    <tr style="background-color: white;">
+                        <td>{{ $i }}</td>
+                        <td>00100{{ $i }}</td>
+                        <td>{{ ['Arga Pratama', 'Keisha Anindya', 'Dimas Fadlan', 'Nayla Putri', 'Rizqy Maulana', 'Alika Salsabila', 'Revan Aditya', 'Tania Ramadhani', 'Ilham Setiawan', 'Vania Lestari'][$i-1] }}</td>
+                        <td>{{ ['3590168908650001','359578436678975','3597752345678999','3527865357988752','3578864323456788','3594576445678956','3594733458886544','3598754323456788','3576544567899667','3590776555444332'][$i-1] }}</td>
+                        <td>{{ ['01-03-2004','16-12-2000','31-03-1998','07-10-2006','25-07-2001','23-01-1997','15-08-1978','19-11-2008','13-04-2005','14-02-1999'][$i-1] }}</td>
+                        <td>17-04-2025</td>
+                        <td>Umum</td>
+                        <td>{{ ['dr. Oktavia', 'dr. Erwiyan', 'dr. Laili Fitriana', 'dr. Erwiyan', 'dr. Oktavia Putri', 'dr. Sisil Karina', 'dr. Shofi', 'dr. Arvin Maulana', 'dr. Arvin Maulana', 'dr. Sisil Karina'][$i-1] }}</td>
+                        <td>{{ $i % 3 == 0 ? 'BPJS' : 'Umum' }}</td>
+                        <td>
+                            @if($i <= 5)
+                                <span style="color: #007bff;">Diperiksa</span>
+                            @else
+                                <span style="color: #F5A623;">Antri</span>
+                            @endif
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-primary">Pilih</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
-
 @endsection
