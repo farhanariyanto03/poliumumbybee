75156228808530bb6799d93b5219eb55a0b2ad81
Mirandadws <mirandadws1464@gmail.com>
2025-04-28 22:41:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Master\DoctorController;
 use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\SuratKeteranganSakitController;
 
 Route::get('/', function () {
     return view('main.index');
@@ -13,3 +14,7 @@ Route::get('/{main}/{view}', [PageController::class, 'show']);
 
 Route::resource('pasien', PasienController::class);
 Route::resource('doctors', DoctorController::class);
+
+// route suratSakit
+
+Route::get('/surat-keterangan-sakit', [SuratKeteranganSakitController::class, 'index'])->name('surat.sakit');
