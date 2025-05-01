d9fd4c6d462b4565023000dc8fbd44f01227fea7
Mirandadws <mirandadws1464@gmail.com>
2025-05-01 15:22:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,43 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SuratKeteranganSakitController;
 
-Route::get('/', function () {
-    return view('main.index');
-});
 
-Route::get('/{main}/{view}', [PageController::class, 'show']);
+// Route::get('/', function () {
+//     return view('main.index');
+// });
 
-Route::resource('pasien', PasienController::class);
-Route::resource('doctors', DoctorController::class);
+// Route::get('/{main}/{view}', [PageController::class, 'show']);
 
-// route suratSakit
+// Dashboard
+Route::get('/', function () { return view('dashboard'); })->name('dashboard');
 
-Route::get('/surat-keterangan-sakit', [SuratKeteranganSakitController::class, 'index'])->name('surat.sakit');
+// Pendaftaran
+Route::get('/pendaftaran', [PasienController::class, 'create'])->name('pendaftaran.create');
+Route::post('/pendaftaran', [PasienController::class, 'store'])->name('pendaftaran.store');
+
+// Surat
+Route::get('/surat/sehat', function () { return view('surat.sehat'); })->name('surat.sehat');
+Route::get('/surat/sakit', function () { return view('surat.sakit'); })->name('surat.sakit');
+
+// Poli Umum
+Route::get('/poliumum/antrian', function () { return view('poli.umum.antrian'); });
+Route::get('/poliumum/tindakan', function () { return view('poli.umum.tindakan'); });
+
+// Farmasi
+Route::get('/farmasi/dataobat', function () { return view('farmasi.dataobat'); });
+Route::get('/farmasi/resep', function () { return view('farmasi.resep'); });
+Route::get('/farmasi/pengambilan', function () { return view('farmasi.pengambilan'); });
+
+// Master Data
+Route::get('/masterdata/pasien', function () { return view('masterdata.pasien'); });
+
+// Doctor
+Route::get('/doctor', function () { return view('dokter.index'); })->name('doctor.index');
+
+
+// // route dokter
+// Route::resource('doctors', DoctorController::class);
+
+// // route suratSakit
+
+// Route::get('/surat-keterangan-sakit', [SuratKeteranganSakitController::class, 'index'])->name('surat.sakit');
